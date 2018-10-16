5465f95ea37cac93d7d78cae8640b490ff5570de
Ivonilson <ivonilsoncardoso@hotmail.com>
2018-10-16 20:02:14 -0300

    atualizacao

--- utilizando_PDO.php
+++ utilizando_PDO.php
@@ -3,7 +3,7 @@
 	/*A classe PDO é nativa do PHP, portanto, não é necessário chamar a mesma. Basta fazer a instanciação de um objeto da classe e passar os parâmetros de conexão.*/
 
 	//declando as variáveis que receberão as strings parâmetros do PDO.
-	 $host = "mysql:host=localhost; dbname=db_musicabrasil";
+	 $host = "mysql:host=localhost; dbname=db_teste";
 	 $user = "root";
 	 $pass = "";
 
@@ -11,9 +11,23 @@
 	try {
 		$conn = new PDO($host, $user, $pass);
 		print "CONECTOU...";	
+
 	} catch(PDOException $e){
 		print "NÃO CONECTOU: ".$e->getMessage();
 	}
+
+		$stmtInserir = $conn->prepare("INSERT INTO tb_cidades (nome_cidade, uf) VALUES (?, ?)");
+		$stmtInserir->bindParam(1, "PLANALTINA");
+		$stmtInserir->bindParam(2, "GO");
+
+		if($stmtInserir->execute()){
+			print "<br> Registro Inserido com sucesso...<br>";
+			var_dump($stmtInserir);
+
+		} else {
+			print "<br> Erro ao inserir o registro.<br>";
+			var_dump($stmtInserir);
+		}
 	
 
 
