9d1e6fe25fc6397e0e5215ffc99428e2724ef015
Ivonilson <ivonilsoncardoso@hotmail.com>
2020-02-09 12:20:11 -0300

    constantes magicas

---
+++ constantes_magicas/constantes_magicas.php
@@ -0,0 +1,12 @@
+<?php
+require 'funcoes.php';
+
+//mostra o caminho do arquivo onde a constante mágica foi declarada
+echo __FILE__ . "<br>";
+
+//mostra o número da linha do script no qual a função foi declarada
+echo __LINE__ . "<br>";
+
+//conterá o diretório do arquivo no qual a função foi declarada
+echo __DIR__ . "<br>";
+
