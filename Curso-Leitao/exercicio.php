edb2ee5418b533be88d8055bf409b0aa33e8ebea
Ivonilson <ivonilsoncardoso@hotmail.com>
2020-02-12 10:57:33 -0200

    menu

--- Curso-Leitao/exercicio.php
+++ Curso-Leitao/exercicio.php
@@ -5,17 +5,26 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="recursos\css\estilo.css">
+    <link rel="stylesheet" href="recursos\css\exercicio.css">
     <title>Exercício</title>
 </head>
 
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
     <main class="principal">
         <div class="conteudo">
-           
+           <?php 
+                //include($_GET['dir'] ."/". $_GET['file'] .".php");
+                //include("teste/teste.php");
+                include("{$_GET['dir']}/{$_GET['file']}.php");
+           ?>
         </div>
     </main>
     <footer class="rodape">
