668daa05b51d05a75bc469ebc47a17b028eadb17
unknown <ivonilsoncardoso@hotmail.com>
2020-05-13 22:50:27 -0300

    comentarios

---
+++ Curso-Leitao/basico/comentarios.php
@@ -0,0 +1,12 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+	
+	echo "Estou no PHP";
+	//forma de comentar uma linha
+	# outra forma de comentar uma linha
+	/*
+		comentários de várias linhas
+	*/
+	
+?>
