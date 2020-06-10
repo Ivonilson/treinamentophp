c28fc59856631ae70d778090c83c6b779e133763
unknown <ivonilsoncardoso@hotmail.com>
2020-06-10 13:48:06 -0300

    operadores relacionais

--- Curso-Leitao/controle/operadores_logicos.php
+++ Curso-Leitao/controle/operadores_logicos.php
@@ -1,2 +1,10 @@
 <div class="titulo">Operadores Lógicos</div>
 
+<?php
+
+//Operadores lógicos
+var_dump(true);
+
+echo '<br>';
+//Operador unário NOT(!) (apenas sobre um operando) que inverte o retorno do boolean
+var_dump(!true);
--- Curso-Leitao/controle/operadores_relacionais.php
+++ Curso-Leitao/controle/operadores_relacionais.php
@@ -13,8 +13,10 @@ var_dump(1 >= 1);
 echo '<br>';
 var_dump(4 <= 23);
 echo '<br>';
+//diferente
 var_dump(1 != 1);
 echo '<br>';
+//diferente
 var_dump(1 <> 1);
 echo '<br>';
 
